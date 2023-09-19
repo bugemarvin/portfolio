928b296ce1064fb8dc945a7a8031c75fee3ffdd9
Marvin Kurland <bugemarvin@outlook.com>
2023-09-19 14:53:10 +0300

    working on futher responsiveness

--- src/utils/sections/about.js
+++ src/utils/sections/about.js
@@ -6,7 +6,7 @@ export default function About() {
                 <div className='Section fCol' id="about">
                         <h1 className='Headers fRow'><span className='Logo-form'>Bio</span></h1>
                         <div className='Contents About'>
-                                <p>I am a software engineer with a passion for crafting efficient solutions. I recently graduated with a Software Engineering Certificate, specializing in back-end technologies.</p>
+                                <p>I am a full-stack software engineer with a passion for crafting efficient solutions, specialized in back-end technologies.</p>
                                 <p>My commitment to lifelong learning drives me to stay updated with industry trends and technologies.</p>
                                 <p>I have had the privilege to work on diverse projects, sharpening my skills and adaptability.</p>
                                 <p>I am actively seeking exciting software engineering opportunities. Let us connect and embark on an exciting journey together!</p>
