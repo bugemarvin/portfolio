25f6e905029f0c229945fb91a446bf7a65b3e074
Marvin Kurland <bugemarvin@outlook.com>
2023-09-20 00:52:10 +0300

    fixing eslint issues

--- src/utils/components/slider.js
+++ src/utils/components/slider.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import './slider.css';
 import React, { useState, useEffect } from 'react';
 
--- src/utils/sections/resume.js
+++ src/utils/sections/resume.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import './resume.css';
 
 export default function Resume () {
