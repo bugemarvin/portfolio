675e7b70b5319d9c39ea860d9f81d9022b1aac0d
Marvin Kurland <bugemarvin@outlook.com>
2023-09-19 20:19:17 +0300

    fixed responsiveness of the web site

--- src/utils/sections/contact.js
+++ src/utils/sections/contact.js
@@ -3,7 +3,7 @@ import ContactForm from '../components/contactForm';
 
 export default function Contacts () {
   return (
-    <div className='Section fCol' id='projects'>
+    <div className='Section fCol' id='contacts'>
       <h1 className='Headers fRow'><span className='Logo-form'>Contacts</span></h1>
       <div className='Contents Contact fCol'>
         <h1 className='Headers fCol'>Send me a message ?</h1>
--- src/utils/sections/projects.js
+++ src/utils/sections/projects.js
@@ -1,10 +1,47 @@
 import './projects.css';
+import ImageSlider from '../components/slider';
+import image from '../img/projects/course_finder/images.png';
+import image1 from '../img/projects/course_finder/images1.png';
+import image2 from '../img/projects/course_finder/images2.png';
+import image3 from '../img/projects/course_finder/images3.png';
+import image4 from '../img/projects/course_finder/images4.png';
+import image5 from '../img/projects/course_finder/images5.png';
+import image6 from '../img/projects/course_finder/images6.png';
+
 
 export default function Projects () {
-  return (
-    <div className='Section fCol' id='contacts'>
+
+  const images = [
+    image,
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+  ];
+
+return (
+    <div className='Section fCol' id='projects'>
       <h1 className='Headers fRow'><span className='Logo-form'>Projects</span></h1>
-      <div className='Contents Skills fRow' />
+      <div className='Contents Skill fCol'>
+        <div className='Contain fRow'>
+          <ImageSlider images={images}/>
+          <div className='Description fCol'>
+            <h2 className='Titles'>Course finder</h2>
+            <p className="Details">Course Finder is a web application that allows users to search for free online courses. It is built with React, Express, MongoDB, BullMQ, Redis, Natural and Puppeteer. It consists of a React frontend and a RESTful API backend. The backend is built with Express and MongoDB. It uses BullMQ and Redis to handle background processes, including sending welcome messages to users. Puppeteer is used to scrape data from the online course websites including Udemy, Udacity and Cousera. All parts of the application are containerized using Docker for easy deployment and scaling.</p>
+            <p className="Details Staks">React, Express, MongoDB, BullMQ, Redis, Natural, Puppeteer</p>
+            <ul class="fRow Sub-Menu Staks">
+              <li>
+                <a href="https://coursefinder.marvinkurland.tech" target="_blank" rel="noopener noreferrer" class="Sub-link">Live Demo</a>
+              </li>
+              <li>
+                <a href="https://github.com/ngugimuchangi/CourseFinder" target="_blank" rel="noopener noreferrer" class="Sub-link">Github</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
