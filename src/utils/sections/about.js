ba094a835b686aef9efcc0e7592375b632fdfe12
Marvin Kurland <bugemarvin@outlook.com>
2023-09-19 14:10:28 +0300

    working on futher responsiveness

--- src/utils/sections/about.js
+++ src/utils/sections/about.js
@@ -6,25 +6,11 @@ export default function About() {
                 <div className='Section fCol' id="about">
                         <h1 className='Headers fRow'><span className='Logo-form'>Bio</span></h1>
                         <div className='Contents About'>
-                                <p>I'm a motivated and aspiring software engineer, With a passion
-                                        for crafting efficient and scalable solutions, I am dedicated to 
-                                        honing my skills in the ever-evolving world of software engineering.<br /><br />Recent graduate of Software Engineering 
-                                        Certificate at ALX Africa, with a specific focus on back-end technologies. 
-                                        This comprehensive program has provided me with a strong foundation in software 
-                                        development principles and practices, equipping me with the necessary tools to 
-                                        tackle complex challenges.</p>
-                                <p>Continuously seeking opportunities to expand my knowledge, I am committed to staying updated 
-                                        with the latest industry trends and technologies.
-                                </p>
-                                <p>I have had the privilege to work on various projects, 
-                                        both individually and as part of a team. These experiences have sharpened my ability to thrive 
-                                        in diverse environments and leverage my skills to deliver high-quality solutions.
-                                </p>
-                                <p>I am actively seeking opportunities to contribute to the software engineering field and engage in 
-                                        challenging projects. Whether you want to explore potential collaborations or discuss software 
-                                        engineering topics, I am always open to connecting. Feel free to reach out and let's embark on an 
-                                        exciting journey together.
-                                </p>
+                                <h2 className='Headers>Welcom</h2>
+                                <p>I am a software engineer with a passion for crafting efficient solutions. I recently graduated with a Software Engineering Certificate, specializing in back-end technologies.</p>
+                                <p>My commitment to lifelong learning drives me to stay updated with industry trends and technologies.</p>
+                                <p>I have had the privilege to work on diverse projects, sharpening my skills and adaptability.</p>
+                                <p>I am actively seeking exciting software engineering opportunities. Let us connect and embark on an exciting journey together!</p>
                         </div>
                 </div>
         );
