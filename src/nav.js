25a26ebe9655930be7792f83a5a610524e873da3
Marvin Kurland <bugemarvin@outlook.com>
2024-01-04 22:39:35 +0300

    adding mobile menu

--- src/nav.js
+++ src/nav.js
@@ -1,11 +1,41 @@
 import './utils/css/index.css';
+import './utils/css/navs.css';
+import { useState } from 'react';
+import { FaHome, FaBars, FaUserGraduate, FaProjectDiagram } from "react-icons/fa";
+import { BsInfoSquareFill } from "react-icons/bs";
+import { GiSkills } from "react-icons/gi";
+import { RiContactsFill } from "react-icons/ri";
+import { Link } from 'react-scroll';
 
 export default function Nav() {
+        const [ isMenuOpen, setIsMenuOpen ] = useState(false);
+        function toggle () {
+                setIsMenuOpen((n) => !n);
+        }
         return (
-                <div className="Navigation">
-                <nav>
-                        <h1><span>M</span>arvin_<span>K</span>urland</h1>
-                </nav>
-                </div>
+                <>
+                        <div className="Navigation">
+                                <nav>
+                                        <h1><span>M</span>arvin_<span>K</span>urland</h1>
+                                </nav>
+                        </div>
+                        <div className={`${isMenuOpen ? 'NavigationClose' : 'NavigationOpen'} Navigation__two Navigation`}>
+                                <div className={isMenuOpen ? 'ContainerClose' : 'ContainerOpen'}>
+                                        <button className={`${isMenuOpen ? 'BtnClose' : 'BtnOpen'} Btn`} onClick={toggle}>{isMenuOpen ? 'X' : <FaBars />}</button>
+                                        <ul className={isMenuOpen ? 'MenuItemsClose' : 'MenuItemsOpen'}>
+                                                <li><Link to="header" smooth={true} duration={500} offset={-100} spy={true}><span><FaHome /></span><span className='Link_navs'>Home</span></Link></li>
+                                                <li><Link to="about" smooth={true} duration={500} offset={-100} spy={true}><span><BsInfoSquareFill /></span><span className='Link_navs'>About</span></Link></li>
+                                                <li><Link to="skills" smooth={true} duration={500} offset={-100} spy={true}><span><GiSkills /></span><span className='Link_navs'>Skills</span></Link></li>
+                                                <li><Link to="resume" smooth={true} duration={500} offset={-100} spy={true}><span><FaUserGraduate /></span><span className='Link_navs'>Resume</span></Link></li>
+                                                <li><Link to="projects" smooth={true} duration={500} offset={-100} spy={true}><span><FaProjectDiagram /></span><span className='Link_navs'>Projects</span></Link></li>
+                                                <li><Link to="contacts" smooth={true} duration={500} offset={100} spy={true}><span><RiContactsFill /></span><span className='Link_navs'>Contact</span></Link></li>
+                                        </ul>
+                                        <p>&copy; 2023 All rights reserved</p>
+                                </div>
+                                <nav className={isMenuOpen ? 'NameClose' : 'NameOpen'}>
+                                        <h1><span>M</span>arvin_<span>K</span>urland</h1>
+                                </nav>
+                        </div>
+                </>
         );
 }
