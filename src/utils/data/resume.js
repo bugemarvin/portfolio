7a121c833a8757c35c257ff5b69a95cfee1e501e
Marvin Kurland <bugemarvin@outlook.com>
2024-08-10 22:13:50 +0300

    added resume data in json format

---
+++ src/utils/data/resume.js
@@ -0,0 +1,87 @@
+import Curiate from '../img/logos/curiateco_logo.jpeg';
+import Modtechy from '../img/logos/modtechy_limited_logo.jpeg';
+import Melian from '../img/logos/melian_dialogue_research_logo.jpeg';
+// 'https://cherryfield.com/wp-content/uploads/2020/12/cf-logo.jpg'
+
+export const Experince = [
+  {
+    'logo': Modtechy,
+    'company': 'Modtechy',
+    'date': 'Apr 2024 - Present',
+    'position': 'Frontend Developer',
+    'location': 'Nairobi, Kenya · Hybrid',
+    'experience': [
+      '• Conducted extensive testing to ensure application responsiveness and reported performance weaknesses for resolution by the development team.',
+      '• Stayed updated on emerging technologies through periodicals, expert consultations, and conference attendance.',
+      '• Updated web pages and navigation tools to improve usability and align with client needs.',
+      '• Developed efficient and elegant websites using various programming languages.',
+      '• Enhanced user experience and functionality by optimizing JavaScript and CSS.',
+      '• Pursued continuous learning in web technologies and programming practices.',
+      '• Collaborated within cross-functional teams to refactor and optimize critical code modules.',
+      '• Maintained proficiency in programming languages, consistently delivering high-quality, object-oriented code.',
+      '• Employed industry-standard testing methods to troubleshoot and resolve application flaws.'
+    ]
+  },
+  {
+    'logo': Melian,
+    'company': 'Melian Dialogue Research',
+    'date': 'Feb 2024 - Apr 2024',
+    'position': 'Full Stack Software Engineer',
+    'location': 'London Area, United Kingdom · Remote',
+    'experience': [
+      '• Integrated Restful services and third-party APIs into legacy products and new solutions.',
+      '• Diagnosed and resolved technical issues, implementing effective solutions for optimal performance.',
+      '• Developed clean and efficient code across various programming languages.',
+      '• Provided insights and support to corporate personnel regarding web development and technologies.',
+      '• Applied Agile methodologies to prototype development in collaboration with senior engineers.',
+      '• Contributed to defining project scope and understanding the planning process with engineering teams.'
+    ]
+  },
+  {
+    'logo': Curiate,
+    'company': 'Curiate Company',
+    'date': 'Apr 2024 - Present',
+    'position': 'Full Stack Software Engineer · Internship',
+    'location': 'United States · Remote',
+    'experience': [
+      '• Integrated Restful services and third-party APIs into legacy products and new solutions.',
+      '• Diagnosed and resolved technical issues, implementing effective solutions for optimal performance.',
+      '• Developed clean and efficient code across various programming languages.',
+      '• Provided insights and support to corporate personnel regarding web development and technologies.',
+      '• Applied Agile methodologies to prototype development in collaboration with senior engineers.',
+      '• Contributed to defining project scope and understanding the planning process with engineering teams.'
+    ]
+  },
+  {
+    'logo': 'https://cherryfield.com/wp-content/uploads/2020/12/cf-logo.jpg',
+    'company': 'CherryField',
+    'date': 'Aug 2020 - Sep 2023',
+    'position': 'Merchandiser',
+    'location': 'Nairobi, Kenya',
+    'experience': [
+      '• Managed end-to-end factory operations, including production analysis and data management.',
+      '• Facilitated transparent communication with buyers regarding production progress and shipment statuses.',
+      '• Oversaw raw material costing, ordering, and requisition creation for production items.',
+      '• Maintained a streamlined workflow to consistently meet and exceed production targets.',
+      '• Managed backorders to ensure timely fulfillment of customer requirements.',
+      '• Identified and resolved product issues in collaboration with supervisors or vendors.',
+      '• Ensured compliance with all company safety policies and guidelines.',
+      '• Skillfully operated material handling equipment to facilitate stock movement and organization.',
+      '• Conducted routine inventory counts and audits to ensure inventory accuracy.',
+      '• Leveraged adept navigation of the storage system to facilitate order fulfillment.'
+    ]
+  },
+  {
+    'logo': 'https://cherryfield.com/wp-content/uploads/2020/12/cf-logo.jpg',
+    'company': 'CherryField',
+    'date': ' Jun 2018 - Aug 2020',
+    'position': 'Merchandising Clerk',
+    'location': 'Nairobi, Kenya',
+    'experience': [
+      '• Conducted meticulous data entry and analysis to optimize fabric consumption in production processes.',
+      '• Proactively monitored key performance indicators (KPIs) and initiated actions to meet store goals.',
+      '• Maintained visual merchandising standards through regular updates and seasonal adjustments.',
+      '• Managed inventory levels and product rotations to ensure optimal store presentation and availability.'
+    ]
+  }
+];
